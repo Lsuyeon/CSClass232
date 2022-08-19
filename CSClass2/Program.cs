d46d37f163cdd9d4f2697f420f1bff994b5f8061
Suyeon Lee <s2015@e-mirim.hs.kr>
2022-08-19 11:10:39 +0900

    초기 커밋.

---
+++ CSClass2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass2
+{
+    class Wanted<T>
+    {
+        public T Value; public Wanted(T value)
+        {
+            this.Value = value;
+        }
+    }
+
+    class SquareCalculator
+    {
+        public int this[int i]
+        {
+            get
+            {
+                return i * i;
+            }
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Wanted<string> wantedString = new Wanted<string>("String"); 
+            Wanted<int> wantedInt = new Wanted<int>(52273); 
+            Wanted<double> wantedDouble = new Wanted<double>(52.273);
+            Console.WriteLine(wantedString.Value); Console.WriteLine(wantedInt.Value); 
+            Console.WriteLine(wantedDouble.Value);
+
+
+            SquareCalculator square = new SquareCalculator(); 
+            Console.WriteLine(square[10]);
+
+
+            Console.WriteLine("숫자 입력 : ");
+            int output;
+            bool result = int.TryParse(Console.ReadLine(), out output);
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자 : " + output);
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력해주세요");
+            }
+
+            int x = 0;
+            int y = 0;
+            int vx = 1;
+            int vy = 1;
+
+            Console.WriteLine("현재 좌표 : (" + x + ", " + y + ")");
+            NextPos(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음 좌표 : (" + x + ", " + y + ")");
+        }
+
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
+    }
+}
